fb240c099b9fa7c33967614120ccfbeaa1033824
zeusato <manhquyet88@gmail.com>
2025-11-22 17:11:40 +0700

    sound update

--- vite.config.ts
+++ vite.config.ts
@@ -23,12 +23,12 @@ export default defineConfig(({ mode }) => {
           theme_color: '#ffffff',
           icons: [
             {
-              src: 'pwa-192x192.png',
+              src: 'Logo.png',
               sizes: '192x192',
               type: 'image/png'
             },
             {
-              src: 'pwa-512x512.png',
+              src: 'Logo.png',
               sizes: '512x512',
               type: 'image/png'
             }
